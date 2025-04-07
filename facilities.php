3656ed4d1ed5f1d147672cf7ebf584d241e170f6
SUMIT JADAV <jadavsumit987@gmail.com>
2025-04-07 20:40:15 +0530

    facilities page

---
+++ facilities.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TJ Hotel-Facilities</title>
+
+    <?php require("inc/links.php");?>
+    <style type="text/css">
+      .pop:hover {
+        border-top-color: var(--teal) !important;
+        scale: 1.03;
+        transition: all 0.3s;
+      }
+    </style>
+  </head>
+  <body class="bg-light">
+    <!-- ! Header Section -->
+    <header>
+      <?php  require("inc/header.php");?>
+    </header>
+    <!-- ! Header section end  -->
+
+    <main>
+      <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque fugiat
+          enim culpa expedita nam. <br />
+          Tempore quisquam architecto voluptate aliquid perspiciatis!
+        </p>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div
+              class="bg-white rounded shaow p-4 border-top border-4 border-dark pop"
+            >
+              <div class="d-flex align-items-center mb-2">
+                <img
+                  src="./images/facilities/IMG_43553.svg"
+                  style="width: 40px"
+                />
+                <h5 class="m-0 ms-3">Wi-Fi</h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Praesentium a quo beatae tempora, ut odit distinctio!
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div
+              class="bg-white rounded shaow p-4 border-top border-4 border-dark pop"
+            >
+              <div class="d-flex align-items-center mb-2">
+                <img
+                  src="./images/facilities/IMG_43553.svg"
+                  style="width: 40px"
+                />
+                <h5 class="m-0 ms-3">Wi-Fi</h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Praesentium a quo beatae tempora, ut odit distinctio!
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div
+              class="bg-white rounded shaow p-4 border-top border-4 border-dark pop"
+            >
+              <div class="d-flex align-items-center mb-2">
+                <img
+                  src="./images/facilities/IMG_43553.svg"
+                  style="width: 40px"
+                />
+                <h5 class="m-0 ms-3">Wi-Fi</h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Praesentium a quo beatae tempora, ut odit distinctio!
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div
+              class="bg-white rounded shaow p-4 border-top border-4 border-dark pop"
+            >
+              <div class="d-flex align-items-center mb-2">
+                <img
+                  src="./images/facilities/IMG_43553.svg"
+                  style="width: 40px"
+                />
+                <h5 class="m-0 ms-3">Wi-Fi</h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Praesentium a quo beatae tempora, ut odit distinctio!
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div
+              class="bg-white rounded shaow p-4 border-top border-4 border-dark pop"
+            >
+              <div class="d-flex align-items-center mb-2">
+                <img
+                  src="./images/facilities/IMG_43553.svg"
+                  style="width: 40px"
+                />
+                <h5 class="m-0 ms-3">Wi-Fi</h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Praesentium a quo beatae tempora, ut odit distinctio!
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div
+              class="bg-white rounded shaow p-4 border-top border-4 border-dark pop"
+            >
+              <div class="d-flex align-items-center mb-2">
+                <img
+                  src="./images/facilities/IMG_43553.svg"
+                  style="width: 40px"
+                />
+                <h5 class="m-0 ms-3">Wi-Fi</h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Praesentium a quo beatae tempora, ut odit distinctio!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <footer>
+      <?php require("inc/footer.php");?>
+    </footer>
+  </body>
+</html>
