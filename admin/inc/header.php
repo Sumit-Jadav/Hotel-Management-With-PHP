313277f1b759d72c5a5c179360c51c699b046f25
SUMIT JADAV <jadavsumit987@gmail.com>
2025-04-20 22:05:46 +0530

    Make Active page highlight

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -8,7 +8,7 @@
   class="col-lg-2 bg-dark border-top border-3 border-secondary"
   id="dashboard-menu"
 >
-  <nav class="navbar navbar-expand-lg navbar-dark">
+  <nav  class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid flex-lg-column align-items-stretch">
       <h4 class="mt-2 text-light">ADMIN PANEL</h4>
       <button
@@ -31,10 +31,10 @@
             <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#">Rooms</a>
+            <a class="nav-link text-white" href="rooms.php">Rooms</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" href="#">Users</a>
+            <a class="nav-link text-white" href="users.php">Users</a>
           </li>
           <li class="nav-item">
             <a class="nav-link text-white" href="settings.php">Settings</a>
