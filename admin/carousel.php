3f5015f5a08f687619c139271e1d54dcafc1beec
SUMIT JADAV <jadavsumit987@gmail.com>
2025-04-21 21:38:31 +0530

    desing issue solve

--- admin/carousel.php
+++ admin/carousel.php
@@ -16,7 +16,7 @@
     <div class="container-fluid" id="main-content">
       <div class="row">
         <div class="col-lg-10 ms-auto p-4 overflow-hidden">
-          <h3 class="mb-4">CAROUSELCarousel</h3>
+          <h3 class="mb-4">CAROUSEL</h3>
           <!-- Carousel Team  section -->
 
           <div class="card border-0 shadow mb-4">
