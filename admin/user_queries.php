27abb0b7275222150bb4d00a1b1f6746c489a31d
SUMIT JADAV <jadavsumit987@gmail.com>
2025-04-21 23:02:07 +0530

    Design

--- admin/user_queries.php
+++ admin/user_queries.php
@@ -66,7 +66,7 @@
     <div class="container-fluid" id="main-content">
       <div class="row">
         <div class="col-lg-10 ms-auto p-4 overflow-hidden">
-          <h3 class="mb-4">User Queries</h3>
+          <h3 class="mb-4">USER QUERIES</h3>
           <div class="card border-0 shadow mb-4">
             <div class="card-body">
                 <div class="text-end mb-4">
