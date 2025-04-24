df5e1a1eba7a38c96430df39304e0949a86d77e9
SUMIT JADAV <jadavsumit987@gmail.com>
2025-04-24 16:48:55 +0530

    made dynamic

--- facilities.php
+++ facilities.php
@@ -3,9 +3,9 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>TJ Hotel-Facilities</title>
-
     <?php require("inc/links.php");?>
+    <title><?php echo $settings_r["site_title"]?>-Facilities</title>
+
     <style type="text/css">
       .pop:hover {
         border-top-color: var(--teal) !important;
