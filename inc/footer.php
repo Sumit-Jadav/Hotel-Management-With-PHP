8103a28cbeb5984072b4908f4520100d5668e0cb
SUMIT JADAV <jadavsumit987@gmail.com>
2025-04-24 16:29:52 +0530

    email varification for registeration

--- inc/footer.php
+++ inc/footer.php
@@ -65,4 +65,76 @@
     }
   }
   setActive();
+
+  function alert(type,message,position='body'){
+    let bs_class = (type == 'success') ? 'alert-success' : 'alert-danger';
+    let element = document.createElement('div')
+    element.innerHTML = `
+      <div class="alert ${bs_class} custome-alert alert-dismissible fade show"  role="alert">
+        <strong class="me-3">${message}</strong>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    `
+    if (position == 'body') {
+      
+      document.body.append(element);
+      element.classList.add("custome-alert");
+    }
+    else{
+      document.getElementById(position).appendChild(element);
+      
+    }
+    setTimeout(remAlert,5000);
+  }
+
+  function remAlert() {
+    document.getElementsByClassName("alert")[0].remove();
+  }
+
+  let registe_form = document.getElementById("register-form");
+  registe_form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let data = new FormData();
+    data.append("name",registe_form.elements["name"].value);
+    data.append("email",registe_form.elements["email"].value);
+    data.append("phonenum",registe_form.elements["phonenum"].value);
+    data.append("profile",registe_form.elements["profile"].files[0]);
+    data.append("address",registe_form.elements["address"].value);
+    data.append("pincode",registe_form.elements["pincode"].value);
+    data.append("dob",registe_form.elements["dob"].value);
+    data.append("pass",registe_form.elements["pass"].value);
+    data.append("cpass",registe_form.elements["cpass"].value);
+    data.append("register","");
+    let myModal = document.getElementById("registerModal");
+    let modal = bootstrap.Modal.getInstance(myModal);
+    modal.hide();
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "ajax/login_register.php", true);
+    
+    xhr.onload = function () {
+      if (this.responseText == "pass_mismatch") {
+        alert("danger","Password Mismatch",document.get);
+      }else if(this.responseText == "email_already"){
+        alert("danger","Email Already Registered!!");
+      }else if(this.responseText == "phone_already"){
+        alert("danger","Phone Already Registered!!");
+      }else if(this.responseText == "inv_img"){
+        alert("danger","Invalid Image!!");
+      }else if(this.responseText == "upd_failed"){
+        alert("danger","Upload Failed");
+      }else if(this.responseText == "mail_failed"){
+        alert("danger","Cannot send varification mail!! Server Dpwn!!!");
+      }else if(this.responseText == "inv_failed"){
+        alert("danger","Phone Already Registered!!");
+      }else {
+        alert("success","Registration Successfully. Varification link send to email."); 
+        registe_form.reset();
+      }
+    };
+    xhr.send(data);
+
+
+});
+
 </script>
