886074e9a56942f2743eb6d114d081a29186163c
SUMIT JADAV <jadavsumit987@gmail.com>
2025-04-24 16:50:03 +0530

    Solve phone number already exist query problem

--- ajax/login_register.php
+++ ajax/login_register.php
@@ -58,7 +58,7 @@ if (isset($_POST["register"])) {
     }
 
     // Check if user exist
-    $u_exist = select("SELECT * FROM `user_cred` WHERE `email`=? AND `phonenum` = ? LIMIT 1",[$data["email"],$data["phonenum"]],"ss");
+    $u_exist = select("SELECT * FROM `user_cred` WHERE `email`=? OR `phonenum` = ? LIMIT 1",[$data["email"],$data["phonenum"]],"ss");
 
     if (mysqli_num_rows($u_exist) != 0) {
         $u_exist_fetch =  mysqli_fetch_assoc($u_exist);
