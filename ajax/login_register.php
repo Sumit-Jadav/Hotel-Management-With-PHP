6ae8e7d449da290cd5f8c778817490070377b115
SUMIT JADAV <jadavsumit987@gmail.com>
2025-04-25 17:26:57 +0530

    forgot password complete

--- ajax/login_register.php
+++ ajax/login_register.php
@@ -9,9 +9,25 @@ require("../PHPMailer/Exception.php");
 require("../PHPMailer/PHPMailer.php");
 require("../PHPMailer/SMTP.php");
 
-function send_mail($email,$name,$token){
-    $mail = new PHPMailer(true);
+date_default_timezone_set("Asia/Kolkata");
 
+
+
+function send_mail($email,$token,$type){
+    $page = "";
+    $subject = "";
+    $content = "";
+    $mail = new PHPMailer(true);
+    if ($type == "email_confirmation") {
+        $page = "email_confirm.php";
+        $subject = "Email Verification Link";
+        $content = "verify your account";
+    }
+    else{
+        $page = "index.php";
+        $subject = "Password Reset Link";
+        $content = "reset your password";
+    }
 try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      
@@ -25,14 +41,14 @@ try {
 
     
     $mail->setFrom(FROM_MAIL, MAILER_NAME);
-    $mail->addAddress($email,$name);       
-
+    // $mail->addAddres($email,$name);       
+    $mail -> addAddress($email);
     
-    $url = SITE_URL."email_confirm.php?email=".$email."&token=".$token."&email_confirmation";
+    $url = SITE_URL.$page."?email=".$email."&token=".$token."&".$type;
     //Content
     $mail->isHTML(true);                                 
-    $mail->Subject = 'Account Varification Link';
-    $mail->Body    = 'Click the link to verify your email<br><a href="' . $url . '">CLICK ME</a>';
+    $mail->Subject = $subject;
+    $mail->Body    = "Click the link to $content<br><a href='" . $url . "'>CLICK ME</a>";
 
     if($mail->send()){
         return 1;
@@ -79,7 +95,7 @@ if (isset($_POST["register"])) {
 
     // Send confirmation link to the user
     $token = bin2hex(random_bytes(16));
-   if(!send_mail($data["email"],$data["name"],$token)){
+   if(!send_mail($data["email"],$token,"email_confirmation")){
     echo "Mail Failed";
     exit;
    }
@@ -105,7 +121,7 @@ if (isset($_POST["login"])) {
         exit;
     }else{
         $u_fetch =  mysqli_fetch_assoc($u_exist);
-        // echo ($u_exist_fetch["email"] == $data["email"])? "email_already" : "phone_already";
+       
         if ($u_fetch["is_verified"] == 0) {
             echo "not_verified";
         }
@@ -131,6 +147,52 @@ if (isset($_POST["login"])) {
 
 }
 
+if (isset($_POST["forgot_pass"])) {
+    $data = filteration($_POST);
+    $u_exist = select("SELECT * FROM `user_cred` WHERE `email`=? LIMIT 1",[$data["email"]],"s");
+    if (mysqli_num_rows($u_exist) == 0) {
+        echo "inv_email";
+        exit;
+    }
+    else{
+        $u_fetch =  mysqli_fetch_assoc($u_exist);
+       
+        if ($u_fetch["is_verified"] == 0) {
+            echo "not_verified";
+        }
+        else if($u_fetch["status"] == 0){
+            echo "inactive";
+        }else{
+            // Send Reset link through email
+            $token = bin2hex(random_bytes(16));
+            if(!send_mail($data["email"],$token,"account_recovery")){
+                echo "mail_failed";
+            }else{
+                $date = date("Y-m-d");
+                $query = mysqli_query($con,"UPDATE `user_cred` SET `token`='$token',`t_expire`='$date' WHERE `id`='$u_fetch[id]'");
+                if ($query) {
+                    echo 1;
+                }
+                else{
+                    echo "upd_failed";
+                }
+            }
+        }
 
+    }
+}
+
+if (isset($_POST["recover_user"])) {
+    $data = filteration($_POST);
+    $enc_pass = password_hash($data["pass"],PASSWORD_BCRYPT);
+    $query = "UPDATE `user_cred` SET `password`=?,`token`=?,`t_expire`=? WHERE `email`=? AND `token`=?";
+    $values = [$enc_pass,null,null,$data["email"],$data["token"]];
+    if (update($query,$values,"sssss")) {
+        echo 1;
+    }
+    else{
+        echo "failed";
+    }
+}
 
 ?>
