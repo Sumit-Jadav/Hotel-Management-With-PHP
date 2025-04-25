6a87caf3ee049a3f9e3fc38a8f490961354ea9de
SUMIT JADAV <jadavsumit987@gmail.com>
2025-04-25 17:28:53 +0530

    update send_mail()

--- email_confirm.php
+++ email_confirm.php
@@ -4,9 +4,10 @@
     require("admin/inc/db_config.php");
     if (isset($_GET["email_confirmation"])) {
         $data = filteration($_GET);
-        $query = select("SELECT * FROM `user_cred` WHERE `email`=? AND `token`=? LIMIT 1",[$data["email"],$data["token"]],"ii");
+        $query = select("SELECT * FROM `user_cred` WHERE `email`=? AND `token`=? LIMIT 1",[$data["email"],$data["token"]],"ss");
         if (mysqli_num_rows($query) == 1) {
             $fetch = mysqli_fetch_assoc($query);
+            
             if ($fetch["is_verified"] == 1) {
                 echo "<script>alert('Already Varified!!!')</script>";
                 redirect("index.php");
