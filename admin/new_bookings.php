24d465d385f51e4b83f6c03612ebf6a5f90c9a13
SUMIT JADAV <jadavsumit987@gmail.com>
2025-04-27 19:03:24 +0530

    New bookings section

--- admin/new_bookings.php
+++ admin/new_bookings.php
@@ -19,7 +19,7 @@
     <div class="container-fluid" id="main-content">
       <div class="row">
         <div class="col-lg-10 ms-auto p-4 overflow-hidden">
-          <h3 class="mb-4">USERS</h3>
+          <h3 class="mb-4">NEW BOOKINGS</h3>
 
           <!-- Feature Card -->
           <div class="card border-0 shadow mb-4">
