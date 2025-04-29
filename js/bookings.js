67bcd634d2ebd4ab401221fd2e2dbd23a66b183b
SUMIT JADAV <jadavsumit987@gmail.com>
2025-04-29 16:15:38 +0530

    Review System js added

--- js/bookings.js
+++ js/bookings.js
@@ -15,3 +15,33 @@ function cancel_booking(id) {
     xhr.send("cancel_booking&id=" + id);
   }
 }
+
+let review_form = document.getElementById("review-form");
+function review_room(bid, rid) {
+  review_form.elements["booking_id"].value = bid;
+  review_form.elements["room_id"].value = rid;
+}
+
+review_form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let data = new FormData();
+  data.append("review_form", "");
+  data.append("rating", review_form.elements["rating"].value);
+  data.append("review", review_form.elements["review"].value);
+  data.append("booking_id", review_form.elements["booking_id"].value);
+  data.append("room_id", review_form.elements["room_id"].value);
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "ajax/review_booking_crud.php", true);
+  xhr.onload = function () {
+    if (this.responseText == 0) {
+      let myModal = document.getElementById("reviewModal");
+      let modal = bootstrap.Modal.getInstance(myModal);
+      modal.hide();
+      alert("danger", "Rating & Review Failed!!");
+    } else {
+      window.location.href = "bookings.php?review_status=true";
+    }
+  };
+  xhr.send(data);
+});
